7da461fdca3e33fe66eabd79a4e900ac7bc2ebd1
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-08-01 22:41:31 +0500

    make our team section

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ export default function App() {
       <HeroSection />
       <About />
       <Events />
+      <Team />
     </>
   );
 }
 
 function TopBanner() {
   return (
-    // <!-- top banner -->
+    //  top banner
     <div class="top-banner">
       <div class="container">
         <div class="small-bold-text banner-text">
@@ -92,7 +93,7 @@ function About() {
             src="/img/asset 2.jpeg"
           />
         </div>
-        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16  md:items-start md:text-left items-center text-center  ">
+        <div className="lg:flex-grow md:w-1/2 lg:ypl-24 md:pl-16  md:items-start md:text-left items-center text-center  ">
           <div className="">
             <h1 className="title-font sm:text-4xl  text-3xl mb-5 font-medium text-black font-left">
               About Us
@@ -246,6 +247,115 @@ function Events() {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+function Team() {
+  const teamMember = [
+    {
+      name: "Ali Mumtaz",
+      img: "/img/teamM1.png",
+      post: "MLSA Lead",
+      description:
+        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+    },
+    {
+      name: "Adnan Sultan",
+      img: "/img/teamM2.png",
+      post: "TechOps Lead",
+      description:
+        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+    },
+    {
+      name: "Asjad Yousaf",
+      img: "/img/teamM3.png",
+      post: "Logistics Lead",
+      description:
+        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+    },
+    {
+      name: "Ahmad Masood",
+      img: "/img/teamM4.jpg",
+      post: "TechOps Co-Lead",
+      description:
+        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+    },
+  ];
+  return (
+    <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-col text-center w-full mb-20">
+          <h1 class="text-2xl font-medium title-font mb-4 text-gray-900 tracking-widest">
+            OUR TEAM
+          </h1>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+            gentrify, subway tile poke farm-to-table. Franzen you probably
+            haven't heard of them.
+          </p>
+        </div>
+        <div class="flex flex-wrap -m-4">
+          {teamMember.map((member) => (
+            <div class="p-4 lg:w-1/2">
+              <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                <img
+                  alt="team"
+                  class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
+                  src={member.img}
+                />
+                <div class="flex-grow sm:pl-8">
+                  <h2 class="title-font font-medium text-lg text-gray-900">
+                    {member.name}
+                  </h2>
+                  <h3 class="text-gray-500 mb-3">{member.post}</h3>
+                  <p class="mb-4">{member.description}</p>
+                  <span class="inline-flex">
+                    <a class="text-gray-500" href="/">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                      </svg>
+                    </a>
+                    <a class="ml-2 text-gray-500" href="/">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                      </svg>
+                    </a>
+                    <a class="ml-2 text-gray-500" href="/">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                      </svg>
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
