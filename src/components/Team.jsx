bba44a29ba7f3c1b863c9e67c93a125a5c5bb4fc
Ahmad Masood <167793144+AhmadMasood-dev@users.noreply.github.com>
2024-08-09 01:27:00 +0500

    now set all the data and when hover on img it comes up a little bit

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,105 +1,126 @@
-import { teamM1, teamM2, teamM3, teamM4 } from "../assets/images/Image.js";
+import {
+  AliMumtaz,
+  AhmadMasood,
+  AsjadYousaf,
+  SannanAdil,
+  AwaisAli,
+  HamzaKhan,
+  HubabMasood,
+  HumairaNadeem,
+  MuhammadAdnanSultan,
+  UmarFarooq,
+  SafeenaAkhter,
+  TalhaBilal,
+  TatheerZahra,
+  IshfaWaleed,
+  WaseelAhmer,
+} from "../assets/images/Image.js";
 
 export default function Team() {
   const teamMember = [
     {
-      name: "Ali Mumtaz",
-      img: teamM1,
-      role: "MLSA Lead",
-      description:
-        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "M.Adnan Sultan",
+      img: MuhammadAdnanSultan,
+      role: "TechOps Lead",
     },
     {
-      name: "Adnan Sultan",
-      img: teamM2,
-      role: "TechOps Lead",
-      description:
-        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Ahmad Masood",
+      img: AhmadMasood,
+      role: "TechOps Co-Lead",
+    },
+    {
+      name: "Hamza Khan",
+      img: HamzaKhan,
+      role: "Logistics Lead",
     },
     {
       name: "Asjad Yousaf",
-      img: teamM3,
-      role: "Logistics Lead",
-      description:
-        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      img: AsjadYousaf,
+      role: "Logistics Co-Lead",
     },
     {
-      name: "Ahmad Masood",
-      img: teamM4,
-      role: "TechOps Co-Lead",
-      description:
-        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Sannan Adil",
+      img: SannanAdil,
+      role: "Graphics Lead",
     },
     {
-      name: "Ali Mumtaz",
-      img: teamM1,
-      role: "MLSA Lead",
-      description:
-        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Hubab Masood",
+      img: HubabMasood,
+      role: "Graphics Co-Lead",
     },
     {
-      name: "Adnan Sultan",
-      img: teamM2,
-      role: "TechOps Lead",
-      description:
-        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Umar Farooq",
+      img: UmarFarooq,
+      role: "Media Lead",
     },
     {
-      name: "Asjad Yousaf",
-      img: teamM3,
-      role: "Logistics Lead",
-      description:
-        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Talha Bilal",
+      img: TalhaBilal,
+      role: " Media Co-Lead",
     },
     {
-      name: "Ahmad Masood",
-      img: teamM4,
-      role: "TechOps Co-Lead",
-      description:
-        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Humaira Nadeem",
+      img: HumairaNadeem,
+      role: "Content Lead",
     },
     {
-      name: "Ahmad Masood",
-      img: teamM4,
-      role: "TechOps Co-Lead",
-      description:
-        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Waseel Ahmer",
+      img: WaseelAhmer,
+      role: "Content Co-Lead",
     },
     {
-      name: "Ahmad Masood",
-      img: teamM4,
-      role: "TechOps Co-Lead",
-      description:
-        " DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+      name: "Awais Ali",
+      img: AwaisAli,
+      role: "Marketing Lead",
+    },
+    {
+      name: "Ishfa Waleed",
+      img: IshfaWaleed,
+      role: "Marketing Co-Lead",
+    },
+    {
+      name: "Tatheer zahra ",
+      img: TatheerZahra,
+      role: "Women in Tech Lead",
+    },
+    {
+      name: "Safeena akhter",
+      img: SafeenaAkhter,
+      role: "Women in Tech  Co-Lead",
     },
   ];
   return (
-    <section id="our-team" class="bg-gray-100 py-32">
-      <div class="container mx-auto px-4">
-        <h2 class="text-3xl font-bold text-center mb-8 text-primary">
-          Meet Our Team
-        </h2>
-        <div class="grid grid-cols-1  gap-8 place-items-center ">
-          <div class="bg-white  rounded-lg shadow-md p-6 my-6 text-center ">
-            <img
-              src={teamM1}
-              alt="Team Member 1"
-              className=" w-[260px] h-[260px] rounded-full mb-4 object-cover  "
-            />
-            <h3 class="text-xl font-semibold mb-2">Ali Mumtaz</h3>
-            <p class="text-gray-700">MLSA Lead</p>
-          </div>
+    <section className="py-6 dark:bg-gray-100 dark:text-gray-800">
+      <div className="container flex flex-col items-center justify-center p-4 mx-auto space-y-8 sm:p-10">
+        <h1 className="text-4xl font-bold leading-none text-center sm:text-5xl text-primary">
+          Our team
+        </h1>
+        <p className="max-w-2xl text-center dark:text-gray-600">
+          At a assumenda quas cum earum ut itaque commodi saepe rem aspernatur
+          quam natus quis nihil quod, hic explicabo doloribus magnam neque,
+          exercitationem eius sunt!
+        </p>
+        <div className="flex flex-col justify-center m-8 text-center">
+          <img
+            alt=""
+            className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500  shadow-md shadow-black transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+            src={AliMumtaz}
+          />
+          <p className="text-xl font-semibold leading-tight">Ali mumtaz</p>
+          <p className="dark:text-gray-600">Lead</p>
         </div>
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8">
+        <div className="flex flex-row flex-wrap justify-center">
           {teamMember.map((member) => (
-            <div class="bg-white  rounded-lg shadow-md p-6 my-6 text-center flex justify-center flex-col">
+            <div className="flex flex-col justify-center m-8 text-center ">
               <img
+                alt=""
+                className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500  shadow-md shadow-black transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-200"
                 src={member.img}
-                alt="Team Member 1"
-                className=" w-[160px] h-[160px] rounded-full mb-4 object-cover  self-center"
               />
-              <h3 class="text-xl font-semibold mb-2">{member.name}</h3>
-              <p class="text-gray-700">{member.role}</p>
+              <p className="text-xl font-semibold leading-tight">
+                {member.name}
+              </p>
+              <p className="dark:text-gray-600">{member.role}</p>
             </div>
           ))}
         </div>
