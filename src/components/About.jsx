14a13ff6a472ae56e2de57042ad673807aaab7a1
Muhammad Adnan Sultan <info.adnansultan@gmail.com>
2024-09-21 04:09:08 +0500

    Website Version 3.0 Minor Bug Fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,16 +17,10 @@ export default function About() {
               About Us
             </h1>
             <p className="mb-5 leading-relaxed">
-              We are a dynamic, hyper focused community supported by Microsoft
-              mainly operating in the Qau University. Our mission is to empower
-              the masses through advanced technological education and encourage
-              innovation.
+            The Microsoft Learn Student Ambassadors (MLSA) at Quaid-i-Azam University (QAU) is a dynamic community dedicated to empowering students through technology and collaborative learning. Our mission is to bridge the gap between academia and the tech industry by providing opportunities for students to enhance their skills, engage in hands-on projects, and connect with industry professionals. Through workshops, hackathons, and networking events, we foster an environment where creativity and innovation thrive, encouraging students to explore new technologies and apply their knowledge in real-world scenarios.
             </p>
             <p className="mb-8 leading-relaxed">
-              We are a dynamic, hyper focused community supported by Microsoft
-              mainly operating in the Qau University. Our mission is to empower
-              the masses through advanced technological education and encourage
-              innovation.
+As part of a global initiative, we strive to create an inclusive platform that welcomes students of all backgrounds and experience levels. By participating in MLSA QAU, members gain valuable insights, develop essential technical skills, and build a strong network within the tech community. Together, we are committed to promoting a culture of learning, collaboration, and growth, ensuring that every student can thrive in today’s rapidly evolving technological landscape. Join us in our journey to inspire, educate, and make a positive impact!
             </p>
           </div>
           <Button name="Learn More" styles="secondary-button " />
